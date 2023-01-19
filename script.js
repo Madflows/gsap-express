4266b07fc2e3e276cf3baa92d7f19a87c593cabd
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-19 15:36:22 +0100

    stagger special property

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-gsap.defaults({ duration: 1 });
+gsap.defaults({ duration: 1, ease: "linear" });
+
 
 gsap.to(".rocket", {
-  y: "100",
-  fill: "green",
-  stagger: 1,
-  borderRadius: "300",
-});
+    repeat: -1,
+    yoyo: true,
+    y: "100",
+    borderRadius: 30,
+    stagger: {
+        each: 0.2,
+        from: "center"
+    }
+})
+
+
+// Yoyo: repeat: -1 makes it loop
+/* gsap.to(".rocket", {
+    repeat: -1,
+    yoyo: true,
+    stagger: 1,
+    borderRadius: 100,
+    y: "100"
+}) */
+
+
+// From To
+// gsap.fromTo(
+//   ".rocket",
+//   {
+//     y: "100",
+//     borderRadius: "300",
+//   },
+//   {
+//     y: 0,
+//     stagger: 1,
+//     borderRadius: "10",
+//   }
+// );
