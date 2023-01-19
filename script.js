19ff4e59e8b4b854ed108c4e8f9ea817e597b128
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-19 15:59:46 +0100

    looking good

--- script.js
+++ script.js
@@ -1,38 +1,12 @@
 gsap.defaults({ duration: 1, ease: "linear" });
 
 
-gsap.to(".rocket", {
-    repeat: -1,
-    yoyo: true,
-    y: "100",
+gsap.from(".left", {
+    opacity: 0,
     borderRadius: 30,
     stagger: {
-        each: 0.2,
-        from: "center"
+        amount: 0.3,
+        from: "end"
     }
 })
 
-
-// Yoyo: repeat: -1 makes it loop
-/* gsap.to(".rocket", {
-    repeat: -1,
-    yoyo: true,
-    stagger: 1,
-    borderRadius: 100,
-    y: "100"
-}) */
-
-
-// From To
-// gsap.fromTo(
-//   ".rocket",
-//   {
-//     y: "100",
-//     borderRadius: "300",
-//   },
-//   {
-//     y: 0,
-//     stagger: 1,
-//     borderRadius: "10",
-//   }
-// );
