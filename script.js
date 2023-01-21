72f32dead84a4a1f7c0afa97e6506e3983c86e8c
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-21 18:37:26 +0100

    done with hover effect class

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-const item = document.querySelector(".item");
-const dot = document.querySelector(".dot");
-const text = document.querySelector(".text");
+const items = document.querySelectorAll(".item");
 
 gsap.defaults({ ease: "easeInOut", duration: 0.3 });
 
@@ -10,16 +8,25 @@ animation
   .from(".slide-bg", { xPercent: "100", duration: "0.5" })
   .to(".slide-bg", { xPercent: "-100", duration: "0.5" }, "+=1");
 
+
+
+
+
+items.forEach((item) => {
+    let text = item.querySelector('.text')
+    let dot = item.querySelector('.dot')
+
 const tl = gsap
   .timeline({
     paused: true,
   })
-  .to(".text", { color: "white", x: 10 })
-  .to(".dot", {backgroundColor: "#f93", scale:1.5}, 0);
+  .to(text, { color: "white", x: 10, scale: 1.3, transformOrigin: "left center" })
+  .to(dot, { backgroundColor: "#f93", scale: 1.5 }, 0);
 
-item.addEventListener("mouseenter", () => {
-  tl.play();
-});
-item.addEventListener("mouseleave", () => {
-  tl.reverse();
-});
+    item.addEventListener("mouseenter", () => {
+      tl.play();
+    });
+    item.addEventListener("mouseleave", () => {
+      tl.reverse();
+    });
+})
