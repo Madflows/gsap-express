5822defc34fa40abc5c23ea34fcc6eef48eb538e
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-21 18:17:17 +0100

    first class completed

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-const play = document.getElementById('play');
-const pause = document.getElementById('pause');
-const reverse = document.getElementById('reverse');
+const item = document.querySelector(".item");
+const dot = document.querySelector(".dot");
+const text = document.querySelector(".text");
 
+gsap.defaults({ ease: "easeInOut", duration: 0.3 });
 
-gsap.defaults({ duration: 1, ease: "easeInOut" });
+var animation = gsap.timeline();
 
+animation
+  .from(".slide-bg", { xPercent: "100", duration: "0.5" })
+  .to(".slide-bg", { xPercent: "-100", duration: "0.5" }, "+=1");
+
+const tl = gsap
+  .timeline({
+    paused: true,
+  })
+  .to(".text", { color: "white", x: 10 })
+  .to(".dot", {backgroundColor: "#f93", scale:1.5}, 0);
+
+item.addEventListener("mouseenter", () => {
+  tl.play();
+});
+item.addEventListener("mouseleave", () => {
+  tl.reverse();
+});
