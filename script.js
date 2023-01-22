5fd7c2870d5bcdf04a8264c789003a8c7148e059
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-22 20:23:24 +0100

    setup project #1

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-const play = document.getElementById('play');
-const pause = document.getElementById('pause');
-const reverse = document.getElementById('reverse');
-
-
 gsap.defaults({ duration: 1, ease: "easeInOut" });
 
+const animation = gsap.timeline();
+
+animation
+    .from(".slide-bg", {
+        xPercent: 100
+    })
+    .to(".slide-bg", {
+        xPercent: -100
+    }, "+=1")
+    .from(".image", {opacity: 0, yPercent: 100})
+    .from(".slide-up", {
+        opacity: 0,
+        stagger: 0.1,
+        yPercent: "-10"
+    }, "<1")
