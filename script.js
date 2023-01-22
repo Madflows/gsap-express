35dfa2b0dd59b0698d718e7615f8ef1568223a0c
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-22 20:02:04 +0100

    done with the constant pulse

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const items = document.querySelectorAll(".item");
-
 gsap.defaults({ ease: "easeInOut", duration: 0.3 });
 
 var animation = gsap.timeline();
@@ -8,25 +6,22 @@ animation
   .from(".slide-bg", { xPercent: "100", duration: "0.5" })
   .to(".slide-bg", { xPercent: "-100", duration: "0.5" }, "+=1");
 
+const cta = document.querySelector(".cta");
 
+//create a repeating scale tween
+const scaleTween = gsap.to(cta, {
+  scale: 1,
+  repeat: 15,
+  yoyo: true,
+  paused: true,
+});
 
+//create a repeating scale tween
+cta.addEventListener("mouseenter", function () {
+  scaleTween.restart();
+});
 
-
-items.forEach((item) => {
-    let text = item.querySelector('.text')
-    let dot = item.querySelector('.dot')
-
-const tl = gsap
-  .timeline({
-    paused: true,
-  })
-  .to(text, { color: "white", x: 10, scale: 1.3, transformOrigin: "left center" })
-  .to(dot, { backgroundColor: "#f93", scale: 1.5 }, 0);
-
-    item.addEventListener("mouseenter", () => {
-      tl.play();
-    });
-    item.addEventListener("mouseleave", () => {
-      tl.reverse();
-    });
-})
+cta.addEventListener("mouseleave", function () {
+    scaleTween.pause()
+    gsap.to(cta, {scale: 0.8})
+});
