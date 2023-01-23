0c4da7d470c5e03db533f8d4dc284257a4515013
Folarin Lawal <lawalfolarin6@gmail.com>
2023-01-23 12:12:31 +0100

    letter by letter animation

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-gsap.registerPlugin(TextPlugin)
+gsap.config({ trialWarn: false });
+gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(SplitText);
+
+// LETTER BY LETTER ANIMATION
+let split;
+let animation = gsap.timeline({});
+
+function init() {
+  gsap.set(".wrapper", { autoAlpha: 1 });
+  split = new SplitText("h2", { type: "chars,words" });
+  // console.log(split)
+  animation.from(split.chars, {
+    opacity: 0,
+    stagger: 0.05,
+    y: 50,
+    ease: "back(4)",
+  });
+  GSDevTools.create({ animation: animation });
+}
+
+window.addEventListener("load", init);
+
+/* 
+=== TYPEWRITER ANIMATION ===
+
 gsap.defaults({ duration: 3, ease: "power1.in"});
 
 let animation = gsap.timeline();
@@ -14,3 +39,5 @@ animation
     repeat: "1",
     yoyo: true,
   });
+
+*/
