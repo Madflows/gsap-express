51958eb38157cd5c075de280352aa8096d5b1e02
Folarin Lawal <lawalfolarin6@gmail.com>
2023-02-11 13:38:42 +0100

    fixed slight styling issues

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ ScrollSmoother.create({
 
 const cardTL = gsap.timeline({
   defaults: {
-    ease: "power",
+    ease: "power4",
   },
   // repeat: -1,
   // yoyo: true,
@@ -18,16 +18,23 @@ const cardTL = gsap.timeline({
   onStart: () => {
     console.log("start");
   },
-  
+  scrollTrigger: {
+    trigger: ".card",
+    // scrub: true,
+    // markers: true,
+    start: "top center-=10%",
+    end: "center center-=20%",
+  },
 });
 
 cardTL
   .from(".card", {
     autoAlpha: 0,
     opacity: 0,
-    y: "20",
+    y: "60",
+    rotate: -20,
     stagger: {
-      each: 0.8,
+      each: 0.4,
     },
   })
   .from(
@@ -35,15 +42,16 @@ cardTL
     {
       autoAlpha: 0,
       scale: 0.5,
-      rotate: 360,
-      ease: "power",
+      ease: "power2",
       stagger: {
         each: 0.4,
       },
       scrollTrigger: {
         trigger: ".card p",
-        markers: true,
         scrub: true,
+        markers: true,
+        start: "top center",
+        end: "center center-=20%",
       },
     },
     "-=.3"
@@ -59,7 +67,7 @@ headings.forEach((heading) => {
     duration: 1,
     scrollTrigger: {
       trigger: heading,
-      markers: true,
+      // markers: true,
       scrub: true,
       // Trigger scoller
       start: "top 80%"
