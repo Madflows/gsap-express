e26af508ea87f9bb85d9154bdb3e6d9c79f00329
Folarin Lawal <lawalfolarin6@gmail.com>
2023-02-11 11:42:39 +0100

    added minified gsap scripts

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const play = document.getElementById('play');
-const pause = document.getElementById('pause');
-const reverse = document.getElementById('reverse');
-
-
 gsap.defaults({ duration: 1, ease: "easeInOut" });
 
+const cardTL = gsap.timeline({
+  defaults: {
+    ease: "power",
+  },
+});
+
+cardTL.from(".card", {
+  autoAlpha: 0,
+  opacity: 0,
+  y: "-10",
+  stagger: 0.2
+})
